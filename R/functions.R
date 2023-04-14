415409859ab5cdadd64a43d75d6115346c838557
Kenneth S. Reyes <kenneth.rejes@gmail.com>
2023-04-14 13:13:18 +0800

    new post

--- R/functions.R
+++ R/functions.R
@@ -30,3 +30,19 @@ end_mark <- function(center = FALSE) {
     return("![](/images/logo-icon.png){.endmark}")
   }
 }
+
+pikachu <- function(center = FALSE) {
+  if (center == TRUE) {
+    return("<p style='text-align:center'>![](/images/pikachu.png){.endmark}</p>")
+  } else {
+    return("![](/images/pikachu.png){.endmark}")
+  }
+}
+
+squint <- function(center = FALSE) {
+  if (center == TRUE) {
+    return("<p style='text-align:center'>![](/images/squint.png){.endmark}</p>")
+  } else {
+    return("![](/images/squint.png){.endmark}")
+  }
+}
